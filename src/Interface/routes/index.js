f3c987f6d100abca8b6bb14b2924eb4d9667736c
a104437ana <a104437@alunos.uminho.pt>
2025-05-10 00:49:24 +0100

    Adicionar footer e começar barra de navegação

--- src/Interface/routes/index.js
+++ src/Interface/routes/index.js
@@ -3,15 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('home');
+  var date = new Date().toLocaleString('pt-PT', { hour12: false });
+  res.render('home',{title: "I am ...", date: date});
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login');
+  var date = new Date().toLocaleString('pt-PT', { hour12: false });
+  res.render('login',{title: "Erro", date: date});
 });
 
 router.get('/signup', function(req, res, next) {
-  res.render('signup');
+  var date = new Date().toLocaleString('pt-PT', { hour12: false });
+  res.render('signup',{title: "Erro", date: date});
 });
 
 module.exports = router;
