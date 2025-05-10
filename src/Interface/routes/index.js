4b4ea73c6265eae22b98f48620bb9bc7aafe4911
a104437ana <a104437@alunos.uminho.pt>
2025-05-10 18:25:07 +0100

    Barra de navegação melhorada

--- src/Interface/routes/index.js
+++ src/Interface/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/home', function(req, res, next) {
   var date = new Date().toLocaleString('pt-PT', { hour12: false });
-  res.render('home',{title: "I am ...", date: date});
+  res.render('home',{title: "I am ... (in bits and bytes)", date: date});
 });
 
 router.get('/login', function(req, res, next) {
