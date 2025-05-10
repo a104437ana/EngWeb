fa250f22d530a6d3872df293ba595e0a486990e6
a104437ana <a104437@alunos.uminho.pt>
2025-05-10 18:43:30 +0100

    Página Inicial de apresentação da aplicação feita

--- src/Interface/routes/index.js
+++ src/Interface/routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  var date = new Date().toLocaleString('pt-PT', { hour12: false });
+  res.render('home',{title: "O Meu Eu Digital", date: date});
+});
+
 router.get('/home', function(req, res, next) {
   var date = new Date().toLocaleString('pt-PT', { hour12: false });
   res.render('home',{title: "I am ... (in bits and bytes)", date: date});
