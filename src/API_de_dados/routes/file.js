3ddb604fe242736a48b842e14d42189ef1799abe
a104263 <a104263@alunos.uminho.pt>
2025-06-01 15:36:03 +0100

    Mostrar ficheiro de texto

--- src/API_de_dados/routes/file.js
+++ src/API_de_dados/routes/file.js
@@ -11,7 +11,6 @@ function now() {
 }
 
 router.get('/:id', Auth.validateGetFile, async function(req, res, next) {
-  var date = now();
   const file = await File.findById(req.params.id);
   if (!file) return res.status(404).send('Ficheiro não encontrado');
   const filePath = path.resolve(file.path);
